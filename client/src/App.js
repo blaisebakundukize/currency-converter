a5d44e6678c74b2a554bebddfcb98c4b79a7587c
Blaise Bakundukize <blaisebakundukize@gmail.com>
2020-07-27 13:25:37 +0200

    added kubernetes yaml files

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    exchangeRates: [],
+  };
+
+  componentDidMount() {
+    this.fetchExchangeRates();
+  }
+
+  async fetchExchangeRates() {
+    const exchangeRates = await axios.get("/api/currency/exchange-rates");
+    this.setState({ exchangeRates: exchangeRates });
+  }
+  render() {
+    console.log(this.state.exchangeRates);
+    return (
+      <div>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>currency Converter</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
