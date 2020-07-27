842b3e5925138de0b64ef7bdeb23d3e595d149a9
Blaise Bakundukize <blaisebakundukize@gmail.com>
2020-07-27 17:39:59 +0200

    refactor action for receiving exchange rates

--- client/src/actions/exchangeRates.js
+++ client/src/actions/exchangeRates.js
@@ -2,10 +2,16 @@ import axios from "axios";
 
 export const RECEIVE_EXCHANGE_RATES = "RECEIVE_EXCHANGE_RATES";
 
-export const receiveExchangeRates = (exchangeRates) => ({
-  type: RECEIVE_EXCHANGE_RATES,
-  exchangeRates,
-});
+export const receiveExchangeRates = (exRates) => {
+  const exchangeRates = {};
+  for (let i = 0; i < exRates.length; i++) {
+    exchangeRates[exRates[i].id] = exRates[i];
+  }
+  return {
+    type: RECEIVE_EXCHANGE_RATES,
+    exchangeRates,
+  };
+};
 
 export const handleReceiveExchangeRates = () => {
   return (dispatch) => {
