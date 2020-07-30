9d1de9f77ca21abe5fba2018fc2a42c2c3b92f72
Blaise Bakundukize <blaisebakundukize@gmail.com>
2020-07-30 18:25:27 +0200

    changed deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-docker build -t bblaise/currency-converter-client:latest -t bblaise/currency-converter-client:$SHA -f ./client/Dockerfile ./client
-docker build -t bblaise/currency-converter-server:latest -t bblaise/currency-converter-server:$SHA -f ./server/Dockerfile ./server
-
-docker push bblaise/currency-converter-client:latest
-docker push bblaise/currency-converter-server:latest
+docker build -t bblaise/currency-converter-client:$SHA -t bblaise/currency-converter-client:latest -f ./client/Dockerfile ./client
+docker build -t bblaise/currency-converter-server:$SHA -t bblaise/currency-converter-server:latest -f ./server/Dockerfile ./server
 
 docker push bblaise/currency-converter-client:$SHA
 docker push bblaise/currency-converter-server:$SHA
+
+docker push bblaise/currency-converter-client:latest
+docker push bblaise/currency-converter-server:latest
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=bblaise/currency-converter-server:$SHA
