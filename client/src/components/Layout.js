82ab93b841f1d0553b2ef5e1c4692dc80004244c
Blaise Bakundukize <blaisebakundukize@gmail.com>
2020-08-06 15:39:32 +0200

    added logout functionality

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import Nav from "./Nav";
+import { LoginReset } from "../actions/login";
 
 class Layout extends Component {
+  handleLogout = () => {
+    this.props.dispatch(LoginReset());
+  };
   render() {
+    const { isAuthenticated } = this.props;
+    let logoutBlock = null;
+    if (isAuthenticated === true) {
+      logoutBlock = (
+        <span className='logout' onClick={this.handleLogout}>
+          Logout
+        </span>
+      );
+    }
     return (
       <div className='container'>
         <header className='header'>
           <h1>Currency Converter</h1>
           <Nav />
+          {logoutBlock}
         </header>
         <main className='main-content'>{this.props.children}</main>
       </div>
@@ -16,4 +31,8 @@ class Layout extends Component {
   }
 }
 
-export default Layout;
+const mapStateToProps = ({ login }) => ({
+  isAuthenticated: login.token !== null,
+});
+
+export default connect(mapStateToProps)(Layout);
