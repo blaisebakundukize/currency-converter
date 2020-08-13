10065d661268d3b7ba9fe9ffc7d2a182f36e288e
Blaise Bakundukize <blaisebakundukize@gmail.com>
2020-08-13 15:21:12 +0200

    added reducer cases for creating base currency

--- client/src/reducers/baseCurrency.js
+++ client/src/reducers/baseCurrency.js
@@ -1,15 +1,49 @@
-import { RECEIVE_BASE_CURRENCIES } from "../actions/baseCurrencies";
+import {
+  RECEIVE_BASE_CURRENCIES,
+  CREATE_BASE_CURRENCY_FAIL,
+  CREATE_BASE_CURRENCY_SUCCESS,
+  CREATE_BASE_CURRENCY_START,
+} from "../actions/baseCurrencies";
 
-const baseCurrencies = (state = {}, action) => {
+const initialState = {
+  bases: null,
+  loadingCreatingBaseCurrency: false,
+  errorCreatingBaseCurrency: null,
+};
+
+const baseCurrency = (state = initialState, action) => {
   switch (action.type) {
     case RECEIVE_BASE_CURRENCIES:
       return {
         ...state,
-        ...action.baseCurrencies,
+        bases: {
+          ...action.baseCurrencies,
+        },
+      };
+    case CREATE_BASE_CURRENCY_START:
+      return {
+        ...state,
+        loadingCreatingBaseCurrency: true,
+        errorCreatingBaseCurrency: null,
+      };
+    case CREATE_BASE_CURRENCY_SUCCESS:
+      return {
+        ...state,
+        bases: {
+          ...state.bases,
+          [action.baseCurrency.id]: action.baseCurrency,
+        },
+        loadingCreatingBaseCurrency: false,
+      };
+    case CREATE_BASE_CURRENCY_FAIL:
+      return {
+        ...state,
+        loadingCreatingBaseCurrency: false,
+        errorCreatingBaseCurrency: action.error,
       };
     default:
       return state;
   }
 };
 
-export default baseCurrencies;
+export default baseCurrency;
